1f99cbdd4dc3daf8dd7ab306084279848e16309f
sofi <abrilyanez123@gmail.com>
2024-01-23 13:33:34 -0300

    modified app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.listen(port, () =>{
     console.log(`Servidor up on port: http://localhost:${port}`);
 });
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "views/home.html"));
 });
 app.get("/register", (req, res) => {
